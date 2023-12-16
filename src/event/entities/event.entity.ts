76604f6c177de13042f421f523add679912d0044
quassbottle <quassbottleyt@gmail.com>
2023-12-16 03:49:42 +0300

    fix event model

--- src/event/entities/event.entity.ts
+++ src/event/entities/event.entity.ts
@@ -2,6 +2,7 @@ import { Prisma, Event } from "@prisma/client";
 import { ImageModel } from "src/storage/entity/image.entity";
 
 export class EventModel implements Event {
+  banner_id: number;
   id: number;
   title: string;
   description: string;
