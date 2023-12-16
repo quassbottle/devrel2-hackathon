a071fce8fe5fc380aded88b21ee7f251de0c65c7
quassbottle <quassbottleyt@gmail.com>
2023-12-17 00:44:34 +0300

    fix

--- src/event/entities/event-join-request.entity.ts
+++ src/event/entities/event-join-request.entity.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { $Enums, EventJoin } from "@prisma/client";
 
 export class EventJoinModel implements EventJoin {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   created_at: Date;
+
+  @ApiProperty()
   status: $Enums.ModerationStatus;
+
+  @ApiProperty()
   user_id: number;
+
+  @ApiProperty()
   event_id: number;
 }
