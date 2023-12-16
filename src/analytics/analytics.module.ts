3ef98689fe0a17654d07e72a2b394a7e86a48d28
quassbottle <quassbottleyt@gmail.com>
2023-12-16 05:57:15 +0300

    fix

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 
 @Module({
-  controllers: [AnalyticsController],
-  providers: [AnalyticsService],
-  exports: [AnalyticsService]
+  controllers: [AnalyticsController]
 })
 export class AnalyticsModule {}
