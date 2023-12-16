ef3ad34ccb893b76bc6499aca0a640999663ac79
quassbottle <quassbottleyt@gmail.com>
2023-12-16 13:29:55 +0300

    analytics gateway

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { CreateAnalyticsDto } from './dto/create-analytics.dto';
 import { UpdateAnalyticsDto } from './dto/update-analytics.dto';
 import { HttpService } from '@nestjs/axios';
 import axios from 'axios';
+import { ApiBody, ApiParam } from '@nestjs/swagger';
+import { AnalyticsRecommendTagsDto } from './dto/recommend-tags.dto';
 
 @Controller('analytics')
 export class AnalyticsController {
@@ -12,8 +14,26 @@ export class AnalyticsController {
   
   @Get('tags/all/stat')
   async getTags() {
-    console.log(this.host);
-    return axios.get(this.host);
+    return (await axios.get('http://ml-api:1488/tags/all/stat')).data;
+  }
+
+  @ApiParam({
+    type: Number,
+    name: 'id',
+  })
+  @Get('events/:id')
+  async getEventById(@Param('id', ParseIntPipe) id) {
+    return (await axios.get('http://ml-api:1488/events/' + id)).data;
+  }
+
+  
+  @ApiBody({
+    type: AnalyticsRecommendTagsDto
+  })
+  @Post('tags')
+  async getRecByTags(@Body() dto: AnalyticsRecommendTagsDto) {
+    const res = await axios.post('http://ml-api:1488/tags/', { ...dto } );
+    return res.data;
   }
 
   // fetchData(): Observable<AxiosResponse<any>> {
